02605956efbf01c7a17779dec10b79c27892296b
Vlad Ovčaruk <vladovcharyk@gmail.com>
2024-04-21 23:49:45 +0300

    final

--- src/contexts/ResultContext.ts
+++
@@ -1,9 +0,0 @@
-import { createContext } from 'react'
-import {IResultPlaneContext} from "../types/ResultPlane.types.ts";
-
-
-const ResultContext = createContext<IResultPlaneContext>({
-	results: [],
-	setResults: () => {},
-})
-export default ResultContext
